69e6472c7df3d1e336310dbb86df7fbc2a329388
Kostas <kaisopos@users.noreply.github.com>
2025-04-18 18:28:37 -0700

    [Evaluation] Convenience function for standard config retrieval (#1644)

--- src/oumi/cli/alias.py
+++ src/oumi/cli/alias.py
@@ -46,6 +46,35 @@ _ALIASES: dict[str, dict[AliasType, str]] = {
     "llama4-maverick": {
         AliasType.INFER: "oumi://configs/recipes/llama4/inference/maverick_instruct_together_infer.yaml",
     },
+    # Hosted models.
+    "claude-3-5-sonnet": {
+        AliasType.INFER: "oumi://configs/apis/anthropic/infer_claude_3_5_sonnet.yaml",
+        AliasType.EVAL: "oumi://configs/apis/anthropic/eval_claude_3_5_sonnet.yaml",
+    },
+    "claude-3-7-sonnet": {
+        AliasType.INFER: "oumi://configs/apis/anthropic/infer_claude_3_7_sonnet.yaml",
+        AliasType.EVAL: "oumi://configs/apis/anthropic/eval_claude_3_7_sonnet.yaml",
+    },
+    "gemini-1-5-pro": {
+        AliasType.INFER: "oumi://configs/apis/gemini/infer_gemini_1_5_pro.yaml",
+        AliasType.EVAL: "oumi://configs/apis/gemini/eval_gemini_1_5_pro.yaml",
+    },
+    "gpt-4o": {
+        AliasType.INFER: "oumi://configs/apis/openai/infer_gpt_4o.yaml",
+        AliasType.EVAL: "oumi://configs/apis/openai/eval_gpt_4o.yaml",
+    },
+    "gpt-o1-preview": {
+        AliasType.INFER: "oumi://configs/apis/openai/infer_gpt_o1_preview.yaml",
+        AliasType.EVAL: "oumi://configs/apis/openai/eval_gpt_o1_preview.yaml",
+    },
+    "llama-3-3-70b": {
+        AliasType.INFER: "oumi://configs/apis/vertex/infer_llama_3_3_70b.yaml",
+        AliasType.EVAL: "oumi://configs/apis/vertex/eval_llama_3_3_70b.yaml",
+    },
+    "llama-3-1-405b": {
+        AliasType.INFER: "oumi://configs/apis/vertex/infer_llama_3_1_405b.yaml",
+        AliasType.EVAL: "oumi://configs/apis/vertex/eval_llama_3_1_405b.yaml",
+    },
 }
 
 
