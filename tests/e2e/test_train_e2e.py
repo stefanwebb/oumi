b47f3438951033ab55fff0f544d94f8884e6c936
William Zeng <10782997+wizeng23@users.noreply.github.com>
2025-06-18 16:41:06 -0700

    Upgrade accelerate and peft (#1774)

--- tests/e2e/test_train_e2e.py
+++ tests/e2e/test_train_e2e.py
@@ -599,16 +599,10 @@ def test_train_multimodal_fsdp_4gpu_80gb(test_config: TrainTestConfig, tmp_path:
     )
 
 
-@requires_gpus(count=4, min_gb=39.0)
+@requires_gpus(count=4, min_gb=79.0)
 @pytest.mark.parametrize(
     "test_config",
     [
-        TrainTestConfig(
-            test_name="train_grpo_tldr_qwen2_500m",
-            config_path=(get_configs_dir() / "examples" / "grpo_tldr" / "train.yaml"),
-            max_steps=3,
-            save_steps=3,
-        ),
         TrainTestConfig(
             test_name="train_grpo_letter_counting",
             config_path=(
@@ -626,7 +620,7 @@ def test_train_multimodal_fsdp_4gpu_80gb(test_config: TrainTestConfig, tmp_path:
 )
 @pytest.mark.e2e
 @pytest.mark.multi_gpu
-def test_train_grpo_4gpu_40gb(test_config: TrainTestConfig, tmp_path: Path):
+def test_train_grpo_4gpu_80gb(test_config: TrainTestConfig, tmp_path: Path):
     _test_train_impl(
         test_config=test_config,
         tmp_path=tmp_path,
